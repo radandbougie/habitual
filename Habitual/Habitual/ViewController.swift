c9d8652d95f2aa2f433003ad2c6cbb5c2835dd52
radandbougie <mcgheekandice@gmail.com>
2018-12-05 13:54:02 -0800

    restarted

---
+++ Habitual/Habitual/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Habitual
+//
+//  Created by Kandy M on 12/5/18.
+//  Copyright © 2018 Kandy M. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
